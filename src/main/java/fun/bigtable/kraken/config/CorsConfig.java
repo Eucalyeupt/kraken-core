6e2863a5c2ca3122ceebdd54693bcc0fe39f466d
zhaomingming <zmm951022@outlook.com>
2024-06-28 16:30:17 +0800

    fix(*): bug处理

--- src/main/java/fun/bigtable/kraken/config/CorsConfig.java
+++ src/main/java/fun/bigtable/kraken/config/CorsConfig.java
@@ -20,13 +20,14 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         //允许所有域名进行跨域调用
-        config.addAllowedOrigin("*");
+//        config.addAllowedOrigin("*");
         //允许跨越发送cookie
         config.setAllowCredentials(true);
         //放行全部原始头信息
         config.addAllowedHeader("*");
         //允许所有请求方法跨域调用
         config.addAllowedMethod("*");
+        config.addAllowedOriginPattern("*");
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
