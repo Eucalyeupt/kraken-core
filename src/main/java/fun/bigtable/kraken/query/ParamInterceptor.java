71dfb76872503471663f0035febf2d6bc8224519
zhaomingming <zmm951022@outlook.com>
2024-10-09 17:28:49 +0800

    feat(mybatis插件): 判断tlog是否存在

--- src/main/java/fun/bigtable/kraken/query/ParamInterceptor.java
+++ src/main/java/fun/bigtable/kraken/query/ParamInterceptor.java
@@ -14,6 +14,7 @@ import org.apache.ibatis.reflection.MetaObject;
 import org.apache.ibatis.reflection.SystemMetaObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.stereotype.Component;
 
 import java.sql.Connection;
@@ -26,6 +27,7 @@ import java.util.Properties;
  * #sqlId;traceId
  */
 @Component
+@ConditionalOnClass(TLogContext.class)
 @Intercepts({
         @Signature(type = StatementHandler.class, method = "prepare", args = {Connection.class, Integer.class})
 })
